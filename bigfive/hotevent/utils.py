4073684627eeecfe7a6b12b9fa86416e2cc5c440
BingquLee <bingqulee@gmail.com>
2019-03-22 16:19:03 +0800

    BingquLee

--- bigfive/hotevent/utils.py
+++ bigfive/hotevent/utils.py
@@ -152,10 +152,10 @@ def get_geo(event_id,geo,s, e):
         else:
             geo_dic[city] += 1
     # 通过省条数排名
-
+    if geo == '中国':
+        geo_dic.update({'国外': abroad_count})
     result= {'city':geo_dic,'rank':[]}
     result['rank'] = [{i[0]:i[1]} for i in sorted(geo_dic.items(), key=lambda x: x[1], reverse=True)[:14]]
-    result['rank'].append({'海外': abroad_count})
     return result
 
 
