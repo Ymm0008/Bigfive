2c62ae1c7dc4e552ca9022955603f6e9e1b59cbc
ibs9668 <31496735+ibs9668@users.noreply.github.com>
2019-03-25 10:58:29 +0800

    lwx

--- bigfive/cron/portrait/user/cron_user.py
+++ bigfive/cron/portrait/user/cron_user.py
@@ -7,7 +7,7 @@ from global_utils import *
 from user_ip import get_user_activity
 from user_topic import get_user_topic
 from user_domain import get_user_domain
-#from user_political import get_user_political
+from user_political import get_user_political
 from user_text_analyze import get_word_analysis
 from user_emotion import cal_user_emotion
 from user_social import cal_user_social
@@ -45,40 +45,40 @@ def user_portrait(uid, start_date,end_date):
     #print(weibo_data_dict)
     
     print('Calculating user position...')
-    #get_user_activity(uid,start_date,end_date)
+    get_user_activity(uid,start_date,end_date)
 
     print('Calculating user topic...')
-    #get_user_topic(uid,start_date,end_date)
+    get_user_topic(uid,start_date,end_date)
 
     print('Calculating user domain...')
-    #get_user_domain(uid,start_date,end_date)
+    get_user_domain(uid,start_date,end_date)
 
     print('Calculating user activity...')
     get_user_weibo_type(uid,weibo_data_dict,start_date,end_date)
 
     print('Calculating user political...')
-    #get_user_political(uid, start_date,end_date)
+    get_user_political(uid, start_date,end_date)
 
     print('Calculating word analysis...')
-    #get_word_analysis(uid,start_date,end_date)
+    get_word_analysis(uid,start_date,end_date)
 
     print('Calculating user emotion...')
-    emo_time_s = time.time()
+    #emo_time_s = time.time()
     cal_user_emotion(uid,weibo_data_dict)
-    emo_time_e = time.time()
-    print('Calculating user emotion time:',emo_time_e-emo_time_s)  
+    #emo_time_e = time.time()
+    #print('Calculating user emotion time:',emo_time_e-emo_time_s)  
 
     print('Calculating user social...')
-    s_s_time = time.time()
-    #cal_user_social(uid,weibo_data_dict)
-    s_e_time = time.time()
-    print('Calculating user social time:',s_e_time-s_s_time)
+    #s_s_time = time.time()
+    cal_user_social(uid,weibo_data_dict)
+    #s_e_time = time.time()
+    #print('Calculating user social time:',s_e_time-s_s_time)
 
     print('Calculating user influence...')
-    i_s_time = time.time()
-    #cal_user_influence(uid,weibo_data_dict)
-    i_e_time = time.time()
-    print('Calculating user influence time:',i_e_time-i_s_time)
+    #i_s_time = time.time()
+    cal_user_influence(uid,weibo_data_dict)
+    #i_e_time = time.time()
+    #print('Calculating user influence time:',i_e_time-i_s_time)
     
 if __name__ == '__main__':
     user_portrait("1663765234","2016-11-13","2016-11-27")
