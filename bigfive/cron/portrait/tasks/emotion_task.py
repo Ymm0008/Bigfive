33df72b52f637ec506ae06d7bcdbe78a43268f15
lifengzhi <lifengzhi@buaa.edu.cn>
2019-04-15 22:00:29 +0800

    lfz

--- bigfive/cron/portrait/tasks/emotion_task.py
+++ bigfive/cron/portrait/tasks/emotion_task.py
@@ -5,6 +5,7 @@ sys.path.append('../../')
 sys.path.append('../')
 sys.path.append('../user/')
 sys.path.append('../user/sentiment_classification')
+from multiprocessing import Pool
 from config import *
 from time_utils import *
 from global_utils import *
@@ -33,10 +34,40 @@ def daily_user_emotion(date):
             weibo_data_dict = get_weibo_data_dict(uid, date,date)
             cal_user_emotion(uid,weibo_data_dict)
 
+def multi_daily_user_emotion(date):    #从Redis取出任务逐个计算
+    while True:
+        uid = redis_r.rpop('emotion_task_%s' % date)
+        if uid == None:
+            break
+        uid = uid.decode('utf-8')  # redis存的是utf-8类型，Python3要解码一下
+        print(uid,date)
+        weibo_data_dict = get_weibo_data_dict(uid, date, date)
+        cal_user_emotion(uid, weibo_data_dict)
+
+def multi_main_emotion(date):
+    #任务推入redis
+    date = ts2date(int(date2ts(date)) - DAY)   #注意这里的取昨天的操作，算历史别算错了
+    redis_r.delete('emotion_task_%s' % date)
+    user_generator = get_user_generator("user_information", {"query":{"bool":{"must":[{"match_all":{}}]}}}, 100000)
+    for res in user_generator:
+        userlist = [i['_source']['uid'] for i in res]
+        redis_r.lpush('emotion_task_%s' % date, *userlist)
+    print('emotion task is set...')
+
+    #多进程计算加快速度
+    p = Pool(8)
+    for i in range(8):
+        p.apply_async(multi_daily_user_emotion, args=(date,))
+    p.close()
+    p.join()   #主进程推动子进程，如果不加join主进程很快结束，子进程也无法进行，join会让主进程一直等待子进程
+    print('calculate over...')
+
 
 if __name__ == '__main__':
-    for date in get_datelist_v2('2019-03-30','2019-04-11'):
-        daily_user_emotion(date)
+    # for date in get_datelist_v2('2019-03-30','2019-04-11'):
+    #     daily_user_emotion(date)
     # theday = today()
     # print('Calculating user emotion...')
     # daily_user_emotion(theday)
+    date = '2019-04-12'
+    multi_main_emotion(date)
